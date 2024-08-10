b0cc4613aafb7a2fe14925f729e98b6f48955076
Mike White <myquite@gmail.com>
2024-08-10 08:59:48 -0500

    moved this project over from codepen

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("All good here!");
+const addPlayerMoveBtn = document.querySelector(".addPlayerMove");
+const list = document.querySelector(".list");
+
+const whiteMove = document.querySelector(".white");
+const blackMove = document.querySelector(".black");
+
+let count = 0;
+
+const gameRecord = [];
+
+addPlayerMoveBtn.addEventListener("click", () => {
+  let moveNum = (count += 1);
+  let newMove = document.createElement("li");
+  let moveText = `${moveNum}. ${whiteMove.value} ${blackMove.value}`;
+  newMove.innerText = moveText;
+  list.appendChild(newMove);
+  gameRecord.push(moveText);
+  whiteMove.value = "";
+  blackMove.value = "";
+  whiteMove.focus();
+});
+
+//TODO add gameRecord to localStorage so data persist.
+//TODO export copies the game records as a string to paste in chess.com
+//TODO wire up newGame button to clear the gameRecord and inputs.
